6fa5c3be3a2896ee8c8a4ad7c562ff42b7dd8b00
Tom Xue <tomxue0126@gmail.com>
2013-09-16 23:54:53 +0800

    to make a WPF socket demo

---
+++ WpfApplication1/WpfApplication1/ClientWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for ClientWindow.xaml
+    /// </summary>
+    public partial class ClientWindow : Window
+    {
+        Socket c;
+        public ClientWindow()
+        {
+            InitializeComponent();
+            InitClient();
+        }
+
+        private void InitClient()
+        {
+            int port = 2000;
+            string host = "127.0.0.1";
+            IPAddress ip = IPAddress.Parse(host);
+            IPEndPoint ipe = new IPEndPoint(ip, port);//把ip和端口转化为IPEndPoint实例
+            c = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);//创建一个Socket
+
+            ShowText("连接到Socket服务端...");
+
+            c.Connect(ipe);//连接到服务器
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ShowText("发送消息到服务端...");
+                string sendStr = textBox2.Text;
+                byte[] bs = Encoding.ASCII.GetBytes(sendStr);
+                c.Send(bs, bs.Length, 0);
+
+                string recvStr = "";
+                byte[] recvBytes = new byte[1024];
+                int bytes;
+                bytes = c.Receive(recvBytes, recvBytes.Length, 0);//从服务器端接受返回信息
+                recvStr += Encoding.UTF8.GetString(recvBytes, 0, bytes);
+
+                ShowText("服务器返回信息：" + recvStr);
+            }
+            catch (ArgumentNullException ex1)
+            {
+                Console.WriteLine("ArgumentNullException:{0}", ex1);
+            }
+            catch (SocketException ex2)
+            {
+                Console.WriteLine("SocketException:{0}", ex2);
+            }
+        }
+
+        private void ShowText(string text)
+        {
+            txtSockInfo.AppendText(text + "\n");
+        }
+    }
+}
---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SocketListener listener;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            InitServer();
+        }
+
+        private void InitServer()
+        {
+            System.Timers.Timer t = new System.Timers.Timer(2000);
+            //实例化Timer类，设置间隔时间为5000毫秒；
+            t.Elapsed += new System.Timers.ElapsedEventHandler(CheckListen);
+            //到达时间的时候执行事件； 
+            t.AutoReset = true;
+            t.Start();
+        }
+
+        private void CheckListen(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (listener != null && listener.Connection != null)
+            {
+                //label2.Content = listener.Connection.Count.ToString();
+                ShowText("连接数：" + listener.Connection.Count.ToString());
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Thread th = new Thread(new ThreadStart(SocketListen));
+            th.Start();
+        }
+
+        private void SocketListen()
+        {
+            listener = new SocketListener();
+            listener.ReceiveTextEvent += new SocketListener.ReceiveTextHandler(ShowText);
+            listener.StartListen();
+        }
+
+        public delegate void ShowTextHandler(string text);
+        ShowTextHandler setText;
+
+        private void ShowText(string text)
+        {
+            if (System.Threading.Thread.CurrentThread != txtSocketInfo.Dispatcher.Thread)
+            {
+                if (setText == null)
+                {
+                    setText = new ShowTextHandler(ShowText);
+                }
+                txtSocketInfo.Dispatcher.BeginInvoke(setText, DispatcherPriority.Normal, new string[] { text });
+            }
+            else
+            {
+                txtSocketInfo.AppendText(text + "\n");
+            }
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            ClientWindow client = new ClientWindow();
+            client.Show();
+        }
+    }
+
+    public class Connection
+    {
+        Socket _connection;
+
+        public Connection(Socket socket)
+        {
+            _connection = socket;
+        }
+
+        public void WaitForSendData()
+        {
+            while (true)
+            {
+                byte[] bytes = new byte[1024];
+                string data = "";
+
+                //等待接收消息
+                int bytesRec = this._connection.Receive(bytes);
+
+                if (bytesRec == 0)
+                {
+                    ReceiveText("客户端[" + _connection.RemoteEndPoint.ToString() + "]连接关闭...");
+                    break;
+                }
+
+                data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+                ReceiveText("收到消息：" + data);
+
+                string sendStr = "服务端已经收到信息！";
+                byte[] bs = Encoding.UTF8.GetBytes(sendStr);
+                _connection.Send(bs, bs.Length, 0);
+            }
+        }
+
+        public delegate void ReceiveTextHandler(string text);
+        public event ReceiveTextHandler ReceiveTextEvent;
+        private void ReceiveText(string text)
+        {
+            if (ReceiveTextEvent != null)
+            {
+                ReceiveTextEvent(text);
+            }
+        }
+    }
+
+    public class SocketListener
+    {
+        public Hashtable Connection = new Hashtable();
+
+        public void StartListen()
+        {
+            try
+            {
+                //端口号、IP地址
+                int port = 2000;
+                string host = "127.0.0.1";
+                IPAddress ip = IPAddress.Parse(host);
+                IPEndPoint ipe = new IPEndPoint(ip, port);
+
+                //创建一个Socket类
+                Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                s.Bind(ipe);//绑定2000端口
+                s.Listen(0);//开始监听
+
+                ReceiveText("启动Socket监听...");
+
+                while (true)
+                {
+                    Socket connectionSocket = s.Accept();//为新建连接创建新的Socket
+
+                    ReceiveText("客户端[" + connectionSocket.RemoteEndPoint.ToString() + "]连接已建立...");
+
+                    Connection gpsCn = new Connection(connectionSocket);
+                    gpsCn.ReceiveTextEvent += new Connection.ReceiveTextHandler(ReceiveText);
+
+                    Connection.Add(connectionSocket.RemoteEndPoint.ToString(), gpsCn);
+
+                    //在新线程中启动新的socket连接，每个socket等待，并保持连接
+                    Thread thread = new Thread(new ThreadStart(gpsCn.WaitForSendData));
+                    thread.Name = connectionSocket.RemoteEndPoint.ToString();
+                    thread.Start();
+                }
+            }
+            catch (ArgumentNullException ex1)
+            {
+                ReceiveText("ArgumentNullException:" + ex1);
+            }
+            catch (SocketException ex2)
+            {
+                ReceiveText("SocketException:" + ex2);
+            }
+        }
+
+        public delegate void ReceiveTextHandler(string text);
+        public event ReceiveTextHandler ReceiveTextEvent;
+        private void ReceiveText(string text)
+        {
+            if (ReceiveTextEvent != null)
+            {
+                ReceiveTextEvent(text);
+            }
+        }
+    }
+}
